7095d431d27f253a5b66b0298fef53e05b890310
Fiskless <fiskless@gmail.com>
2022-09-12 14:58:31 +0500

    add item model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+
+    name = models.CharField(
+        verbose_name='Наименование товара',
+        max_length=30,
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание товара',
+        help_text='Введите ставку в процентах',
+    )
+    price = models.DecimalField(
+        verbose_name='Стоимость товара',
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.0)]
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
