0774dad1bfe73a9c0eb3ac58d52d090b8eb2631e
Fiskless <fiskless@gmail.com>
2022-09-16 21:06:00 +0500

    add payment for order

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path, include
 
-from .views import ItemDetail, create_session, CancelledView, SuccessView
+from .views import ItemDetail, \
+    create_session_for_item, \
+    create_session_for_order,\
+    CancelledView, \
+    SuccessView, \
+    OrderDetail
 
 urlpatterns = [
-    path('buy/<int:id>/', create_session, name='create_session'),
+    path('buy_item/<int:id>/', create_session_for_item,
+         name='create_session_for_item'),
+    path('buy_item/<int:id>/success/', SuccessView.as_view(),
+         name='successed_payment_item'),
+    path('buy_item/<int:id>/cancelled/', CancelledView.as_view(),
+         name='cancelled_payment_item'),
+    path('buy_order/<int:id>/', create_session_for_order,
+         name='create_session_for_order'),
+    path('buy_order/<int:id>/success/', SuccessView.as_view(),
+         name='successed_payment_order'),
+    path('buy_order/<int:id>/cancelled/', CancelledView.as_view(),
+         name='cancelled_payment_order'),
     path('item/<int:id>/', ItemDetail.as_view(), name='get_item_info'),
-    path('buy/<int:id>/success/', SuccessView.as_view(), name='successed_payment'),
-    path('buy/<int:id>/cancelled/', CancelledView.as_view(), name='cancelled_payment'),
+    path('order/<int:id>/', OrderDetail.as_view(), name='get_order_info'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 import stripe
 from django.conf import settings
+from django.db.models import Sum
 from django.views.generic import TemplateView
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 
-from .models import Item
+from .models import Item, Order
 
 
 class ItemDetail(generics.RetrieveAPIView):
@@ -23,11 +24,30 @@ class ItemDetail(generics.RetrieveAPIView):
             'item': self.object,
             'stipe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY
         },
-                        template_name='item_info.html')
+            template_name='item_info.html')
+
+
+class OrderDetail(generics.RetrieveAPIView):
+    """
+    A view that returns a templated HTML representation of a given user.
+    """
+    queryset = Order.objects.all().annotate(total_price=Sum('item__price'))
+    renderer_classes = [TemplateHTMLRenderer]
+    lookup_field = 'id'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return Response({
+            'total_price': self.object.total_price,
+            'order': self.object,
+            'items': self.object.item.all(),
+            'stipe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY
+        },
+            template_name='order_info.html')
 
 
 @api_view(['GET'])
-def create_session(request, id):
+def create_session_for_item(request, id):
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     order = Item.objects.get(id=id)
@@ -41,6 +61,33 @@ def create_session(request, id):
                     'name': f'Оплата товара {order.name}',
                 },
                 'unit_amount': int(order.price) * 100,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=request.build_absolute_uri('success/'),
+        cancel_url=request.build_absolute_uri('cancelled/')
+    )
+
+    return Response({"session_id": session.id})
+
+
+@api_view(['GET'])
+def create_session_for_order(request, id):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    order = Order.objects. \
+        annotate(total_price=Sum('item__price')). \
+        get(id=id)
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'rub',
+                'product_data': {
+                    'name': f'Оплата заказа № {order.id}',
+                },
+                'unit_amount': int(order.total_price) * 100,
             },
             'quantity': 1,
         }],
